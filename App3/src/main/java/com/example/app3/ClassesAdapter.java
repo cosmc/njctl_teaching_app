9f79aec9bd058498bed7e60c0df128a4f9872029
Ying Quan Tan <ying.quan.tan@gmail.com>
2013-11-16 16:52:54 -0500

    got classes to show in the app

---
+++ App3/src/main/java/com/example/app3/ClassesAdapter.java
@@ -0,0 +1,56 @@
+package com.example.app3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ying on 11/16/13.
+ */
+class ClassesAdapter extends ArrayAdapter {
+
+    public ArrayList<NJCTLClass> classes;
+
+    public void setClasses(ArrayList<NJCTLClass> classes) { this.classes = classes; }
+
+
+    class ViewHolder {
+        TextView classNameTextView;
+        TextView progressTextView;
+        TextView unitsTextView;
+    }
+
+
+    public ClassesAdapter(Context context, int resource, ArrayList<NJCTLClass> classes) {
+        super(context, resource, classes);
+        this.classes = classes;
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.class_row_viewholder, parent, false);
+            holder = new ViewHolder();
+            holder.classNameTextView = (TextView) convertView.findViewById(R.id.class_row_viewholder_class_name_textview);
+            holder.progressTextView = (TextView) convertView.findViewById(R.id.class_row_viewholder_progress_label_textview);
+            holder.unitsTextView = (TextView) convertView.findViewById(R.id.class_row_viewholder_units_textview);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        NJCTLClass aClass = classes.get(position);
+        holder.classNameTextView.setText(aClass.className);
+        holder.unitsTextView.setText("8");
+        holder.progressTextView.setText("In Progress");
+        return convertView;
+    }
+}
+
--- App3/src/main/java/com/example/app3/ClassesFragment.java
+++ App3/src/main/java/com/example/app3/ClassesFragment.java
@@ -1,23 +1,43 @@
 package com.example.app3;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import com.example.app3.ClassesAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by ying on 11/16/13.
  */
+
 public class ClassesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.classes_fragment, container, false);
+        View v = inflater.inflate(R.layout.classes_fragment, container, false);
+        ListView listView = (ListView) v.findViewById(R.id.classes_fragment_listview);
+
+        ArrayList<NJCTLClass> mockClasses = new ArrayList<NJCTLClass>();
+        mockClasses.add(new NJCTLClass("Math"));
+        mockClasses.add(new NJCTLClass("Physics"));
+        mockClasses.add(new NJCTLClass("Chemistry"));
+
+        ClassesAdapter adapter = new ClassesAdapter(getActivity(), 0, mockClasses);
+        listView.setAdapter(adapter);
+        return v;
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
     }
+
+
 }
+
+
